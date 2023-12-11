7476ea7ca352a3a3e10117d28ab3207018324489
Graham <cloudyg79@outlook.com>
2023-12-11 22:10:28 +0000

    Fix modularisation issues

--- scripts/core_functions.py
+++ scripts/core_functions.py
@@ -1,14 +1,14 @@
-def chrome_init():
+import core_modules
+    
+def chrome_download_linked_file(c_url,c_xpath):
     # Setup options for headless Chrome
-    options = Options()
+    options = core_modules.Options()
     options.add_argument("--headless")
     prefs = {"download.default_directory" : "/tmp/downloads/"}
     options.add_experimental_option("prefs",prefs)
 
     # Initialize the Chrome driver
-    driver = webdriver.Chrome(options=options)
-    
-def chrome_download_linked_file(c_url,c_xpath):
+    driver = core_modules.webdriver.Chrome(options=options)
     try:
         driver.get(c_url)
 
@@ -16,10 +16,46 @@ def chrome_download_linked_file(c_url,c_xpath):
 
         downloadfile.click()
 
-        time.sleep(5)
+        core_modules.time.sleep(5)
 
         driver.close()
 
     except Exception as e:
 
         print(e)
+        
+        return -1
+    
+    return 0
+
+def chrome_get_page_content(c_url):
+    # Setup options for headless Chrome
+    options = core_modules.Options()
+    options.add_argument("--headless")
+    options.add_argument("--enable-javascript")
+    prefs = {"download.default_directory" : "/tmp/downloads/"}
+    options.add_experimental_option("prefs",prefs)
+
+    # Initialize the Chrome driver
+    driver = core_modules.webdriver.Chrome(options=options)
+    try:
+        driver.implicitly_wait(10)
+        
+        driver.get(c_url)
+        
+        driver.get(c_url)
+        
+        core_modules.time.sleep(10)
+
+        # Execute JavaScript and get the page content
+        page_content = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+
+        driver.close()
+
+    except Exception as e:
+
+        print(e)
+        
+        return -1
+    
+    return page_content
--- scripts/scanner.py
+++ scripts/scanner.py
@@ -1,28 +1,47 @@
 import core_modules
 import core_functions
 
-core_functions.chrome_init()
-
+#Initialise url and xpath query for source code search
 pwww_url = 'https://publicwww.com/websites/%22color-scheme%3Adark%22/'
 pwww_xpath = '//a[contains(@href,"?export=urls")]'
 
-core_functions.chrome_download_linked_file(pwww_url,pwww_xpath)
-driver.quit()
+# Get liat of URLs matching source code query
+print("Getting sites that have dark mode in source code from publicwww.com")
+rslt = core_functions.chrome_download_linked_file(pwww_url,pwww_xpath)
 
-with open('/tmp/downloads/color-schemedark.txt', 'r') as file:
-    for url in file:
-        # Parse the domain from the URL
-        domain = urlparse(url).netloc
+if rslt == 0: #Success
+    print("Success!")
+    #Loop through URLs in file and get Symantec site categor(ies)y
+    
+    i = 1
+    
+    # Open File
+    print("Processing domains...")
+    with open('/tmp/downloads/color-schemedark.txt', 'r') as file:
+        # Loop through URLs in file
+        for url in file:
+            
+            # Get number of domains in file
+            num_domains = sum(1 for _ in file)            
+            
+            # Parse the domain from the URL
+            domain = core_modules.urlparse(url).netloc
 
-        # Make a request to the Symantec site review page for the domain
-        symantec_url = 'https://sitereview.symantec.com/#/lookup-result/' + domain
+            print("Processing domain",i,"of",num_domains,":",domain)
 
-        core_functions.chrome_init()
-        
-        driver.get(symantec_url)
+            # Setup url for Symantec lookup inc domain to lookup
+            symantec_url = 'https://sitereview.symantec.com/#/lookup-result/' + domain
 
-        # Execute JavaScript and get the page content
-        page_content = driver.execute_script("return document.documentElement.outerHTML")
-
-        # Print the page content
-        print(page_content)
+            # Query Symantec
+            rslt = core_functions.chrome_get_page_content(symantec_url)
+            
+            if rslt == -1:
+                print("Failure getting page content from domain: ",domain," - EXITING")
+                core_modules.sys.exit()
+            else:
+                fname = "/tmp/sites/" + domain + ".txt"
+                print("Success! Writing page content to",fname)
+                f = open (fname, 'w')
+                f.write(rslt)
+                f.close()
+                i = i + 1
